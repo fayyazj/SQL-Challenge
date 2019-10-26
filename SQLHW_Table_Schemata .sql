00eace358e23ab6e80876cbc70a395f9e2aa1ba3
fayyazj <jahin.fayyaz@gmail.com>
2019-10-26 00:56:13 -0400

    Final Push

--- SQLHW_Table_Schemata .sql
+++ SQLHW_Table_Schemata .sql
@@ -1,87 +1,94 @@
--- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+DROP TABLE IF EXISTS Department CASCADE;
+DROP TABLE IF EXISTS Employees CASCADE;
+DROP TABLE IF EXISTS Department_Employee CASCADE;
+DROP TABLE IF EXISTS Department_Manager CASCADE;
+DROP TABLE IF EXISTS Salaries CASCADE;
+DROP TABLE IF EXISTS Titles CASCADE;
 
 -- Department CSV
-
-CREATE TABLE "Department" (
-    "dept_no" VARCHAR   NOT NULL,
-    "dept_name" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_Department" PRIMARY KEY (
-        "dept_no"
-     )
-);
-
--- Department Emp CSV
-CREATE TABLE "Department_Employee" (
-    "emp_no" INT   NOT NULL,
-    "dept_no" VARCHAR   NOT NULL,
-    "from_date" VARCHAR   NOT NULL,
-    "to_date" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_Department_Employee" PRIMARY KEY (
-        "emp_no"
-     )
-);
-
--- Department Manager CSV
-CREATE TABLE "Department_Manager" (
-    "dept_no" VARCHAR   NOT NULL,
-    "emp_no" INT   NOT NULL,
-    "from_date" VARCHAR   NOT NULL,
-    "to_date" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_Department_Manager" PRIMARY KEY (
-        "dept_no"
-     )
+CREATE TABLE Department (
+    dept_no VARCHAR   NOT NULL,
+    dept_name VARCHAR   NOT NULL,
+   PRIMARY KEY (dept_no),
+   UNIQUE      (dept_name)
 );
 
 -- Employees CSV
-CREATE TABLE "Employees" (
-    "emp_no" INT   NOT NULL,
-    "birth_date" VARCHAR   NOT NULL,
-    "first_name" VARCHAR   NOT NULL,
-    "last_name" VARCHAR   NOT NULL,
-    "gender" VARCHAR   NOT NULL,
-    "hire_date" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_Employees" PRIMARY KEY (
-        "emp_no"
-     )
+CREATE TABLE Employees (
+   emp_no INT   NOT NULL,
+   birth_date VARCHAR   NOT NULL,
+   first_name VARCHAR   NOT NULL,
+   last_name VARCHAR   NOT NULL,
+   gender VARCHAR   NOT NULL,
+   hire_date VARCHAR   NOT NULL,
+   PRIMARY KEY (emp_no)
+);
+
+-- Department Emp CSV
+CREATE TABLE Department_Employee (
+    emp_no INT   NOT NULL,
+    dept_no VARCHAR   NOT NULL,
+    from_date VARCHAR   NOT NULL,
+    to_date VARCHAR   NOT NULL,
+   FOREIGN KEY (emp_no)  REFERENCES Employees   (emp_no),
+   FOREIGN KEY (dept_no) REFERENCES Department (dept_no),
+   PRIMARY KEY (emp_no,dept_no)
+);
+
+-- Department Manager CSV
+CREATE TABLE Department_Manager (
+    dept_no VARCHAR   NOT NULL,
+    emp_no INT   NOT NULL,
+    from_date VARCHAR   NOT NULL,
+    to_date VARCHAR   NOT NULL,
+  FOREIGN KEY (emp_no)  REFERENCES Employees (emp_no),
+  FOREIGN KEY (dept_no) REFERENCES Department (dept_no),
+  PRIMARY KEY (emp_no,dept_no)
 );
 
 -- Salaries CSV
-CREATE TABLE "Salaries" (
-    "emp_no" INT   NOT NULL,
-    "salary" INT   NOT NULL,
-    "from_date" VARCHAR   NOT NULL,
-    "to_date" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_Salaries" PRIMARY KEY (
-        "emp_no"
-     )
+CREATE TABLE Salaries (
+    emp_no INT   NOT NULL,
+    salary INT   NOT NULL,
+    from_date VARCHAR   NOT NULL,
+    to_date VARCHAR   NOT NULL,
+   FOREIGN KEY (emp_no) REFERENCES Employees (emp_no),
+   PRIMARY KEY (emp_no, from_date)
 );
+
 
 -- Titles CSV
-CREATE TABLE "Titles" (
-    "emp_no" INT   NOT NULL,
-    "title" VARCHAR   NOT NULL,
-    "from_date" VARCHAR   NOT NULL,
-    "to_date" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_Titles" PRIMARY KEY (
-        "emp_no"
-     )
+CREATE TABLE Titles (
+    emp_no INT   NOT NULL,
+    title VARCHAR   NOT NULL,
+    from_date VARCHAR   NOT NULL,
+    to_date VARCHAR   NOT NULL,
+   FOREIGN KEY (emp_no) REFERENCES Employees (emp_no),
+   PRIMARY KEY (emp_no,title, from_date)
 );
 
-ALTER TABLE "Department_Employee" ADD CONSTRAINT "fk_Department_Employee_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no");
+--Copy in CSVs, please keep in mind that I am using my wife's laptop
+--for this class and thus her name appears instead of mine for filepath
+COPY department(dept_no,dept_name) 
+FROM '/Users/celiakresser/Documents/GitHub/SQL-Challenge/data/departments.csv' 
+DELIMITER ',' CSV HEADER;
 
-ALTER TABLE "Department_Employee" ADD CONSTRAINT "fk_Department_Employee_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "Department" ("dept_no");
+COPY employees(emp_no,birth_date,first_name,last_name,gender,hire_date) 
+FROM '/Users/celiakresser/Documents/GitHub/SQL-Challenge/data/employees.csv' 
+DELIMITER ',' CSV HEADER;
 
-ALTER TABLE "Department_Manager" ADD CONSTRAINT "fk_Department_Manager_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "Department" ("dept_no");
+COPY department_employee(emp_no,dept_no,from_date,to_date) 
+FROM '/Users/celiakresser/Documents/GitHub/SQL-Challenge/data/dept_emp.csv' 
+DELIMITER ',' CSV HEADER;
 
-ALTER TABLE "Department_Manager" ADD CONSTRAINT "fk_Department_Manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no");
+COPY department_manager(dept_no,emp_no,from_date,to_date) 
+FROM '/Users/celiakresser/Documents/GitHub/SQL-Challenge/data/dept_manager.csv' 
+DELIMITER ',' CSV HEADER;
 
-ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no");
+COPY salaries(emp_no,salary,from_date,to_date) 
+FROM '/Users/celiakresser/Documents/GitHub/SQL-Challenge/data/salaries.csv' 
+DELIMITER ',' CSV HEADER;
 
-ALTER TABLE "Titles" ADD CONSTRAINT "fk_Titles_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employees" ("emp_no");
+COPY titles(emp_no,title,from_date,to_date) 
+FROM '/Users/celiakresser/Documents/GitHub/SQL-Challenge/data/titles.csv' 
+DELIMITER ',' CSV HEADER;
